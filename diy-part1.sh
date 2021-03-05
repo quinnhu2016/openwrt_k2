cc2e5da5b61e2337eb93676a5b9d9573dbec05c0
quinnhu2016 <quinnhu@163.com>
2021-03-05 12:50:27 +0800

    Update diy-part1.sh
    add smartdns

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+# git clone https://github.com/pymumu/smartdns.git package/smartdns
+git clone https://github.com/pymumu/smartdns.git package/smartdns
